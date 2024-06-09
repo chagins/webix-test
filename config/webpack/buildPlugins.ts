6afb89745aa6377b3a94e46e52d93f93a09a647f
Yaroslav Chagin <46863533+chagins@users.noreply.github.com>
2024-06-09 18:24:41 +0500

    Initial commit

---
+++ config/webpack/buildPlugins.ts
@@ -0,0 +1,27 @@
+import webpack, { WebpackPluginInstance } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BuildOptions } from './types';
+
+export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
+  const plugins: WebpackPluginInstance[] = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+      chunkFilename: 'css/[name].[contenthash].css',
+    }),
+    new webpack.DefinePlugin({
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin(), new webpack.HotModuleReplacementPlugin());
+  }
+
+  return plugins;
+};
